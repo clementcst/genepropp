297f533c183980c6e21c43f9bf497ef863c10e55
Adamou02 <borasse02@gmail.com>
2023-10-12 14:05:53 +0200

    Ajout header profil

---
+++ genealogic-tree-front/Genepropp/src/app/components/profil-header/profil-header.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-profil-header',
+  templateUrl: './profil-header.component.html',
+  styleUrls: ['./profil-header.component.css']
+})
+export class ProfilHeaderComponent {
+
+}
