241b4759195321f253c00abde5a8161fdc75a1bc
Adam Bouhrara <42783707+Adamou02@users.noreply.github.com>
2023-10-22 20:52:50 +0200

    Ajout page profil

--- genealogic-tree-front/Genepropp/src/app/app.module.ts
+++ genealogic-tree-front/Genepropp/src/app/app.module.ts
@@ -3,19 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavComponent } from 'src/app/components/nav/nav.component';
-import { NavElementComponent } from './components/nav-element/nav-element.component';
+import { NavComponent } from 'src/app/components/nav-folder/nav/nav.component';
+import { NavElementComponent } from './components/nav-folder/nav-element/nav-element.component';
 import { LoginPageComponent } from './pages/login-page/login-page.component';
 import { HomePageComponent } from './pages/home-page/home-page.component';
-import { LoginComponent } from './components/login/login.component';
+import { LoginComponent } from './components/identification-folder/login/login.component';
 import { ContactPageComponent } from './pages/contact-page/contact-page.component';
 import { MyTreePageComponent } from './pages/my-tree-page/my-tree-page.component';
 import { DirectoryPageComponent } from './pages/directory-page/directory-page.component';
 import { ProfilPageComponent } from './pages/profil-page/profil-page.component';
-import { RegistrationComponent } from './components/registration/registration.component';
+import { RegistrationComponent } from './components/identification-folder/registration/registration.component';
 import { RegistrationPageComponent } from './pages/registration-page/registration-page.component';
 import { HomeContentComponent } from './components/home-content/home-content.component';
-import { ProfilHeaderComponent } from './components/profil-header/profil-header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { DirectoryComponent } from './components/directory-folder/directory/directory.component';
+import { DirectoryElementComponent } from './components/directory-folder/directory-element/directory-element.component';
+import { DirectoryContentComponent } from './components/directory-folder/directory-content/directory-content.component';
+import { ContactsComponent } from './components/contact-folder/contacts/contacts.component';
+import { ContactsElementComponent } from './components/contact-folder/contacts-element/contacts-element.component';
+import { ContactsContentComponent } from './components/contact-folder/contacts-content/contacts-content.component';
+import { ContactsChatComponent } from './components/contact-folder/contacts-chat/contacts-chat.component';
+import { ProfilContentComponent } from './components/profil-folder/profil-content/profil-content.component';
+import { ProfilLeftComponent } from './components/profil-folder/profil-left/profil-left.component';
+import { ProfilRigthComponent } from './components/profil-folder/profil-rigth/profil-rigth.component';
 
 
 
@@ -34,7 +44,17 @@ import { ProfilHeaderComponent } from './components/profil-header/profil-header.
     RegistrationComponent,
     RegistrationPageComponent,
     HomeContentComponent,
-    ProfilHeaderComponent
+    FooterComponent,
+    DirectoryComponent,
+    DirectoryElementComponent,
+    DirectoryContentComponent,
+    ContactsComponent,
+    ContactsElementComponent,
+    ContactsContentComponent,
+    ContactsChatComponent,
+    ProfilContentComponent,
+    ProfilLeftComponent,
+    ProfilRigthComponent,
   ],
   imports: [
     BrowserModule,
---
+++ genealogic-tree-front/Genepropp/src/app/components/profil-folder/profil-content/profil-content.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-profil-content',
+  templateUrl: './profil-content.component.html',
+  styleUrls: ['./profil-content.component.css']
+})
+export class ProfilContentComponent {
+
+}
