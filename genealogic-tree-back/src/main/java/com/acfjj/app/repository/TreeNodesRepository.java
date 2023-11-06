c7526c2495d5da63d2bb0c48a71ad1e49b897a34
LegJo <106883191+LegJo@users.noreply.github.com>
2023-11-06 10:17:18 +0100

    j'ai fait dla d mais chu le patron (c moa)

--- genealogic-tree-back/src/main/java/com/acfjj/app/repository/TreeNodesRepository.java
+++
@@ -1,8 +0,0 @@
-package com.acfjj.app.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.acfjj.app.model.TreeNodes;
-
-public interface TreeNodesRepository extends CrudRepository<TreeNodes, Long>{	
-}
