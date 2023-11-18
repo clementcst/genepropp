37beb1c781dd37cc2292a3215ae1148de7cd3989
JordanG2 <115002419+JordanG2@users.noreply.github.com>
2023-11-18 22:26:15 +0100

    Update TestDataCreationController.java

--- genealogic-tree-back/src/main/java/com/acfjj/app/controller/TestDataCreationController.java
+++ genealogic-tree-back/src/main/java/com/acfjj/app/controller/TestDataCreationController.java
@@ -29,7 +29,7 @@ public class TestDataCreationController {
 	
 		
 	@PostMapping("/test/users") 
-	public void addTestUsers() {
+	public response addTestUsers() {
 		List<User> users = new ArrayList<User>(Arrays.asList(new User[] {
 			new User("Bourhara", "Adam", 1, LocalDate.of(2002, 04, 2), "France", "Cergy", "adam@mail", "password1","Sécurité socisse", "Telephone ui", "nationality", "adress", 1234, "profilPictureData64"),
 	        new User("Cassiet", "Clement", 1, LocalDate.of(1899, 07, 9), "Péîs", "Tournant-En-Brie", "clement@mail", "password2", "Sécurité socisse", "Telephone ui", "nationality", "adress", 1234, "profilPictureData64"),
@@ -43,7 +43,7 @@ public class TestDataCreationController {
 	}
 	
 	@PostMapping("/test/trees")
-    public void addTree() {
+    public response addTree() {
 		List<Tree> Trees = new ArrayList<Tree>(Arrays.asList(new Tree[] {
             new Tree("Tree Bourhara", 1),
             new Tree("Tree Cassiet", 0),
@@ -58,7 +58,7 @@ public class TestDataCreationController {
 	
 
     @PostMapping("/test/nodes")
-    public void addNodes() {
+    public response addNodes() {
     	 List<Node> nodes = new ArrayList<Node>(Arrays.asList(new Node[] {
             new Node("Bourhara", "Adam", 1, LocalDate.of(2002, 04, 2), "France", "Cergy", userService.getUser(1), 1, "French", "Some Address", 12345, "Base64Image"),
             new Node("Cassiet", "Clement", 1, LocalDate.of(1899, 07, 9), "Péîs", "Tournant-En-Brie", userService.getUser(2), 2, "Peïsian", "Another Address", 54321, "Base64Image2"),
