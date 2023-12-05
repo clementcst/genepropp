05555e7f57101ee09a980a8dfbf2776c44f86153
JordanG2 <115002419+JordanG2@users.noreply.github.com>
2023-12-05 15:19:19 +0100

    Correction 2e arbre

--- genealogic-tree-back/src/main/java/com/acfjj/app/controller/TestDataCreationController.java
+++ genealogic-tree-back/src/main/java/com/acfjj/app/controller/TestDataCreationController.java
@@ -73,26 +73,26 @@ public class TestDataCreationController {
 		Node exPartner = nodeService.getNode((long) 5);
 
 		List<Response> responses = new ArrayList<>();
-		responses.add(treeController.addLinkedNode(tree.getId(), node.getId(), parent1, 1, "Parent", true));
-		responses.add(treeController.addLinkedNode(tree.getId(), node.getId(), partner, 0, "Partner", true));
-		responses.add(treeController.addLinkedNode(tree.getId(), node.getId(), siblings, 1, "Siblings", true));
+		responses.add(treeController.addLinkedNode(tree.getId(), node.getId(), parent1, 1, "Parent", false));
+		responses.add(treeController.addLinkedNode(tree.getId(), node.getId(), partner, 0, "Partner", false));
+		responses.add(treeController.addLinkedNode(tree.getId(), node.getId(), siblings, 1, "Siblings", false));
 		responses.add(treeController.addLinkedNode(tree.getId(), siblings.getId(), parent1, 1, "Parent", false));
-		responses.add(treeController.addLinkedNode(tree.getId(), node.getId(), exPartner, 0, "ExPartner", true));
+		responses.add(treeController.addLinkedNode(tree.getId(), node.getId(), exPartner, 0, "ExPartner", false));
 
 		PersonInfo personInfo1 = new PersonInfo("Dupont", "Camille", 0, LocalDate.of(2002, 04, 2), "Pays", "Ville",
 				true, "Nationalité", "Adresse", 12345, "Base64Image");
 		Node newNode1 = new Node(personInfo1, userService.getUser(1), 1);
-		responses.add(treeController.addLinkedNode(tree.getId(), node.getId(), newNode1, 1, "Parent", true));
-		responses.add(treeController.addLinkedNode(tree.getId(), newNode1.getId(), parent1, 1, "Partner", false));
-		responses.add(treeController.addLinkedNode(tree.getId(), siblings.getId(), newNode1, 1, "Parent", false));
+		responses.add(treeController.addLinkedNode(tree.getId(), node.getId(), newNode1, 1, "Parent", false));
+		responses.add(treeController.addLinkedNode(tree.getId(), newNode1.getId(), parent1, 1, "Partner", true));
+		responses.add(treeController.addLinkedNode(tree.getId(), siblings.getId(), newNode1, 1, "Parent", true));
 
 		Node newNode2 = new Node("Silva", "Rafael", 1, LocalDate.of(2002, 04, 2), "Pays", "Ville",
 				userService.getUser(1), 1, "Nationalité", "Adresse", 12345, "Base64Image");
-		responses.add(treeController.addLinkedNode(tree.getId(), newNode1.getId(), newNode2, 1, "Parent", true));
+		responses.add(treeController.addLinkedNode(tree.getId(), newNode1.getId(), newNode2, 1, "Parent", false));
 
 		Node newNode3 = new Node("Petrov", "Yuri", 1, LocalDate.of(2002, 04, 2), "Pays", "Ville",
 				userService.getUser(1), 1, "Nationalité", "Adresse", 12345, "Base64Image");
-		responses.add(treeController.addLinkedNode(tree.getId(), newNode1.getId(), newNode3, 1, "Siblings", true));
+		responses.add(treeController.addLinkedNode(tree.getId(), newNode1.getId(), newNode3, 1, "Siblings", false));
 		responses.add(treeController.addLinkedNode(tree.getId(), newNode3.getId(), newNode2, 0, "Parent", true));
 
 		for (Response response : responses) {
@@ -102,60 +102,68 @@ public class TestDataCreationController {
 		}
 		return new Response(responses);
 	}
-	
+
 	@PostMapping("/test/treeTest2")
 	public Response addTreeTest2() {
 		List<Response> responses = new ArrayList<>();
 
-		User userTest = new User("Martin", "Martin", 1, LocalDate.of(2002, 04, 2), "France", "Cergy", "test@mail", "password1",
-				"Sécurité socisse", "Telephone ui", "nationality", "adress", 1234, "https://99designs-blog.imgix.net/blog/wp-content/uploads/2016/03/web-images.jpg?auto=format&q=60&w=1600&h=824&fit=crop&crop=faces");
+		User userTest = new User("Martin", "Y", 1, LocalDate.of(2002, 04, 2), "France", "Cergy", "test@mail",
+				"password1", "Sécurité socisse", "Telephone ui", "nationality", "adress", 1234,
+				"https://99designs-blog.imgix.net/blog/wp-content/uploads/2016/03/web-images.jpg?auto=format&q=60&w=1600&h=824&fit=crop&crop=faces");
 		responses.add(userController.addUser(userTest, null));
 
 		Tree tree = userTest.getMyTree();
-		Node node = nodeService.getPublicNodeByNameAndBirthInfo("Martin", "Martin", LocalDate.of(2002, 04, 2), "France", "Cergy");
-		
-		
-		Node newNode1 = new Node("Dubois", "Sophie", 1, LocalDate.of(1990, 12, 15), "France", "Paris",
-				userService.getUser(5), 0, "Française", "123 Rue de la Paix", 75001, "https://img.freepik.com/photos-premium/panda-deguise-statue-liberte-guerre-chine-etats-unis_238683-236.jpg");
-		responses.add(treeController.addLinkedNode(tree.getId(), node.getId(), newNode1, 1, "partner", false));
-		
-		Node newNode2 = new Node("Garcia", "Carlos", 1, LocalDate.of(1985, 7, 8), "Espagne", "Barcelone",
-				userService.getUser(5), 1, "Espagnol", "456 Carrer de Gràcia", 75001, "https://img.freepik.com/photos-premium/panda-noir-blanc-geant-mange-du-bambou-gros-plan-animalier_304356-320.jpg");
+		Long nodeId = userTest.getRelatedNodeId();
+
+		Node newNode1 = new Node("Dubois", "F", 1, LocalDate.of(1990, 12, 15), "France", "Paris", userTest, 0,
+				"Française", "123 Rue de la Paix", 75001,
+				"https://img.freepik.com/photos-premium/panda-deguise-statue-liberte-guerre-chine-etats-unis_238683-236.jpg");
+		responses.add(treeController.addLinkedNode(tree.getId(), nodeId, newNode1, 1, "partner", false));
+
+		Node newNode2 = new Node("Garcia", "E", 1, LocalDate.of(1985, 7, 8), "Espagne", "Barcelone", userTest, 1,
+				"Espagnol", "456 Carrer de Gràcia", 75001,
+				"https://img.freepik.com/photos-premium/panda-noir-blanc-geant-mange-du-bambou-gros-plan-animalier_304356-320.jpg");
 		responses.add(treeController.addLinkedNode(tree.getId(), newNode1.getId(), newNode2, 1, "Parent", false));
 
-		Node newNode3 = new Node("Schneider", "Elena", 2, LocalDate.of(1995, 3, 21), "Allemagne", "Berlin",
-				userService.getUser(5), 1, "Allemande", "789 Unter den Linden", 10117, "https://img.freepik.com/photos-premium/portrait-homme-affaires-costume-tete-requin-gros-plan-extreme-ia-generative_476612-24667.jpg");
+		Node newNode3 = new Node("Schneider", "B", 2, LocalDate.of(1995, 3, 21), "Allemagne", "Berlin", userTest, 1,
+				"Allemande", "789 Unter den Linden", 10117,
+				"https://img.freepik.com/photos-premium/portrait-homme-affaires-costume-tete-requin-gros-plan-extreme-ia-generative_476612-24667.jpg");
 		responses.add(treeController.addLinkedNode(tree.getId(), newNode1.getId(), newNode3, 1, "Parent", false));
 		responses.add(treeController.addLinkedNode(tree.getId(), newNode3.getId(), newNode2, 1, "exPartner", true));
 
-		Node newNode4 = new Node("Rossi", "Giuseppe", 1, LocalDate.of(1988, 9, 10), "Italie", "Rome",
-				userService.getUser(5), 1, "Italien", "Via del Corso", 75001, "https://img.freepik.com/premium-photo/3d-cartoon-fish-shark-portrait-wearing-clothes-glasses-hat-jacket-standing-front_741910-1595.jpg");
+		Node newNode4 = new Node("Rossi", "A", 1, LocalDate.of(1988, 9, 10), "Italie", "Rome", userTest, 1,
+				"Italien", "Via del Corso", 75001,
+				"https://img.freepik.com/premium-photo/3d-cartoon-fish-shark-portrait-wearing-clothes-glasses-hat-jacket-standing-front_741910-1595.jpg");
 		responses.add(treeController.addLinkedNode(tree.getId(), newNode3.getId(), newNode4, 1, "Partner", false));
-		
-		Node newNode5 = new Node("Smith", "Emily", 2, LocalDate.of(1980, 5, 3), "Royaume-Uni", "Londres",
-				userService.getUser(5), 0, "Anglaise", "1 Buckingham Palace Road", 75001, "https://img.freepik.com/photos-premium/requin-queue-arc-ciel-corne-tete_593294-8770.jpg?w=360");
+
+		Node newNode5 = new Node("Smith", "C", 2, LocalDate.of(1980, 5, 3), "Royaume-Uni", "Londres", userTest, 0,
+				"Anglaise", "1 Buckingham Palace Road", 75001,
+				"https://img.freepik.com/photos-premium/requin-queue-arc-ciel-corne-tete_593294-8770.jpg?w=360");
 		responses.add(treeController.addLinkedNode(tree.getId(), newNode4.getId(), newNode5, 1, "Child", false));
 		responses.add(treeController.addLinkedNode(tree.getId(), newNode3.getId(), newNode5, 1, "Child", true));
 
-		
-		Node newNode6 = new Node("Kim", "Jin", 1, LocalDate.of(1993, 11, 27), "Corée du Sud", "Séoul",
-				userService.getUser(5), 0, "Coréen", "123 Gangnam-daero", 75001, "https://img.freepik.com/photos-premium/licorne-bande-dessinee-criniere-bleue-cheveux-roses-se-tient-fond-blanc_593294-18360.jpg");
+		Node newNode6 = new Node("Kim", "D", 1, LocalDate.of(1993, 11, 27), "Corée du Sud", "Séoul", userTest, 0,
+				"Coréen", "123 Gangnam-daero", 75001,
+				"https://img.freepik.com/photos-premium/licorne-bande-dessinee-criniere-bleue-cheveux-roses-se-tient-fond-blanc_593294-18360.jpg");
 		responses.add(treeController.addLinkedNode(tree.getId(), newNode4.getId(), newNode6, 1, "Child", false));
 		responses.add(treeController.addLinkedNode(tree.getId(), newNode3.getId(), newNode6, 1, "Child", true));
 		responses.add(treeController.addLinkedNode(tree.getId(), newNode6.getId(), newNode5, 1, "siblings", true));
-		
-		Node newNode7 = new Node("Fernandez", "Isabella", 2, LocalDate.of(1982, 6, 14), "Argentine", "Buenos Aires",
-				userService.getUser(5), 1, "Argentine", "456 Avenida 9 de Julio", 75001, "https://img.freepik.com/photos-premium/licorne-bande-dessinee-corne-criniere_81048-17810.jpg");
+
+		Node newNode7 = new Node("Fernandez", "G", 2, LocalDate.of(1982, 6, 14), "Argentine", "Buenos Aires",
+				userTest, 1, "Argentine", "456 Avenida 9 de Julio", 75001,
+				"https://img.freepik.com/photos-premium/licorne-bande-dessinee-corne-criniere_81048-17810.jpg");
 		responses.add(treeController.addLinkedNode(tree.getId(), newNode4.getId(), newNode7, 1, "siblings", false));
-		
-		Node newNode8 = new Node("Ahmed", "Youssef", 1, LocalDate.of(1996, 2, 18), "Égypte", "Le Caire",
-				userService.getUser(5), 0, "Égyptien", "789 Sharia El Tahrir", 75001, "https://img.freepik.com/photos-premium/licorne-bande-dessinee-corne-rose-est-fond-violet_593294-15126.jpg");
-		responses.add(treeController.addLinkedNode(tree.getId(), newNode6.getId(), newNode8, 1, "parent", false));
+
+		Node newNode8 = new Node("Ahmed", "X", 1, LocalDate.of(1996, 2, 18), "Égypte", "Le Caire", userTest, 0,
+				"Égyptien", "789 Sharia El Tahrir", 75001,
+				"https://img.freepik.com/photos-premium/licorne-bande-dessinee-corne-rose-est-fond-violet_593294-15126.jpg");
+		responses.add(treeController.addLinkedNode(tree.getId(), newNode4.getId(), newNode8, 1, "parent", false));
 		responses.add(treeController.addLinkedNode(tree.getId(), newNode7.getId(), newNode8, 1, "parent", true));
 
-		Node newNode9 = new Node("Watanabe", "Aya", 2, LocalDate.of(1987, 4, 5), "Japon", "Tokyo",
-				userService.getUser(5), 1, "Japonaise", "1-1 Marunouchi", 75001, "https://gweb-research-imagen.web.app/compositional/An%20oil%20painting%20of%20a%20British%20Shorthair%20cat%20wearing%20a%20cowboy%20hat%20and%20red%20shirt%20skateboarding%20on%20a%20beach./1_.jpeg");
-		responses.add(treeController.addLinkedNode(tree.getId(), newNode6.getId(), newNode9, 1, "parent", false));
+		Node newNode9 = new Node("Watanabe", "Z", 2, LocalDate.of(1987, 4, 5), "Japon", "Tokyo", userTest, 1,
+				"Japonaise", "1-1 Marunouchi", 75001,
+				"https://gweb-research-imagen.web.app/compositional/An%20oil%20painting%20of%20a%20British%20Shorthair%20cat%20wearing%20a%20cowboy%20hat%20and%20red%20shirt%20skateboarding%20on%20a%20beach./1_.jpeg");
+		responses.add(treeController.addLinkedNode(tree.getId(), newNode4.getId(), newNode9, 1, "parent", false));
 		responses.add(treeController.addLinkedNode(tree.getId(), newNode7.getId(), newNode9, 1, "parent", true));
 		responses.add(treeController.addLinkedNode(tree.getId(), newNode9.getId(), newNode8, 1, "partner", true));
 
--- genealogic-tree-back/src/main/java/com/acfjj/app/service/TreeService.java
+++ genealogic-tree-back/src/main/java/com/acfjj/app/service/TreeService.java
@@ -122,10 +122,10 @@ public class TreeService {
 	public void addNodeToTree(Tree tree, Node node, int privacy, int depth) {
 		if (tree != null && node != null) {
 			Set<TreeNodes> treeNodes = tree.getTreeNodes();
-			if (treeNodes.contains(null) || treeNodes == null) {
+			if (treeNodes == null || treeNodes.size()== 1) {
 				treeNodes = new HashSet<>();
 			}
-			boolean associationExists = treeNodes.stream().anyMatch(treeNode -> treeNode.getNode().equals(node));
+			boolean associationExists = treeNodes.stream().anyMatch(treeNode -> !Objects.isNull(treeNode) && treeNode.getNode().equals(node));
 			if (!associationExists) {
 				personInfoRepository.save(node.getPersonInfo());
 				nodeRepository.save(node);
@@ -147,6 +147,7 @@ public class TreeService {
 			if (treeNode.getTree().getId() == treeId)
 				depth = treeNode.getDepth();
 		}
+		System.out.println();
 		addNodeToTree(tree, parent, privacy, depth + 1);
 		if (node.getParent1() == null || node.getParent1Id().equals(parent.getId())) {
 			node.setParent1(parent);
