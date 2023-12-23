0ce333c5f401e6c42ff7ef5334fa6e651cab25ad
Adam Bouhrara <42783707+Adamou02@users.noreply.github.com>
2023-12-23 18:13:09 +0100

    feat: post pour la partie droite de la page profil

--- genealogic-tree-front/Genepropp/src/app/components/profil-folder/profil-inputs/profil-inputs.component.ts
+++ genealogic-tree-front/Genepropp/src/app/components/profil-folder/profil-inputs/profil-inputs.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-profil-inputs',
@@ -6,9 +6,14 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./profil-inputs.component.css']
 })
 export class ProfilInputsComponent {
-  @Input() input:any;
+  @Input() input: any;
+  @Output() valueChanged = new EventEmitter<any>();
 
   isCursorNotAllowed(): boolean {
     return this.input.disabled === true;
+  }
+
+  onValueChanged(event: any) {
+    this.valueChanged.emit({ label: this.input.label, value: event.target.value });
   }
 }
