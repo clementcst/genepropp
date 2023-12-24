39b5e63d9b2dd854518b42e91b71f9e5a03ead48
Adam Bouhrara <42783707+Adamou02@users.noreply.github.com>
2023-12-24 21:50:41 +0100

    fix: secu access from url

--- genealogic-tree-front/Genepropp/src/app/pages/contact-page/contact-page.component.ts
+++ genealogic-tree-front/Genepropp/src/app/pages/contact-page/contact-page.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CookieService } from 'ngx-cookie-service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-contact-page',
   templateUrl: './contact-page.component.html',
   styleUrls: ['./contact-page.component.css']
 })
-export class ContactPageComponent {
+export class ContactPageComponent implements OnInit {
+  isUserIdValid: boolean = true;
 
+  constructor(private cookieService: CookieService, private router: Router) { }
+
+  ngOnInit(): void {
+    if (!this.cookieService.get('userId')) {
+      this.isUserIdValid = false;
+      this.router.navigate(['/']);
+    }
+  }
 }
