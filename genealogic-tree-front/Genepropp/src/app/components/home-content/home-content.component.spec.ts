9db4182b622527fa8f67678d3f28ed3b4853f646
Clément <82080806+clementcst@users.noreply.github.com>
2023-12-26 23:11:40 +0100

    Add files via upload

---
+++ genealogic-tree-front/Genepropp/src/app/components/home-content/home-content.component.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { HomeContentComponent } from './home-content.component';
+
+describe('HomeContentComponent', () => {
+  let component: HomeContentComponent;
+  let fixture: ComponentFixture<HomeContentComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [HomeContentComponent]
+    });
+    fixture = TestBed.createComponent(HomeContentComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
