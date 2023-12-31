c6b98e5509134c4ae67a2ef171473702d22a3f80
Clément <82080806+clementcst@users.noreply.github.com>
2023-12-31 11:52:10 +0100

    Update directory.component.ts
    ajout de la search bar

--- genealogic-tree-front/Genepropp/src/app/components/directory-folder/directory/directory.component.ts
+++ genealogic-tree-front/Genepropp/src/app/components/directory-folder/directory/directory.component.ts
@@ -9,15 +9,26 @@ import { UserService } from '../../../services/user/user.service';
 export class DirectoryComponent implements OnInit {
 
   users: any[] = [];
+  filteredUsers: any[] = [];
+  searchText: string = '';
 
-  constructor(private userService : UserService ) { 
-    this.userService = userService;
-  }
+  constructor(private userService: UserService) { }
 
   ngOnInit(): void {
     this.userService.getUsers().subscribe((data) => {
-      this.users = data.value; 
+      this.users = data.value;
+      this.filteredUsers = this.users;
     });
   }
 
+  onSearchChange() {
+    // Vérifie si les utilisateurs sont disponibles avant de filtrer
+    if (this.users.length > 0) {
+      // Filtrer les utilisateurs en fonction du texte saisi
+      this.filteredUsers = this.users.filter(user =>
+        user.firstName.toLowerCase().startsWith(this.searchText.toLowerCase()) ||
+        user.lastName.toLowerCase().startsWith(this.searchText.toLowerCase())
+      );
+    }
+  }
 }
