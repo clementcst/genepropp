0155cf7089031a342811059b90e3ab14f24cc591
LegJo <106883191+LegJo@users.noreply.github.com>
2024-01-05 01:27:50 +0100

    Delete genealogic-tree-back directory

--- genealogic-tree-back/src/main/java/com/acfjj/app/repository/PersonInfoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.acfjj.app.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.acfjj.app.model.PersonInfo;
-
-public interface PersonInfoRepository extends CrudRepository<PersonInfo, Long>{
-	
-}
--- genealogic-tree-back/src/main/java/com/acfjj/app/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.acfjj.app.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.acfjj.app.model.User;
-
-public interface UserRepository extends CrudRepository<User, Long>{
-	
-}
