fff701b4fc4918bff54cbb24836c5765a2554aa2
Adam Bouhrara <42783707+Adamou02@users.noreply.github.com>
2024-01-05 23:57:55 +0100

    fix: undo merge

--- genealogic-tree-back/javadoc/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.acfjj.app"},{"l":"com.acfjj.app.controller"},{"l":"com.acfjj.app.model"},{"l":"com.acfjj.app.repository"},{"l":"com.acfjj.app.service"},{"l":"com.acfjj.app.utils"},{"l":"com.acfjj.test"},{"l":"com.acfjj.test.model"},{"l":"com.acfjj.test.service"}];updateSearchResults();
